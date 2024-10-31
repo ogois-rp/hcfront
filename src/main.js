1676ab418cea35b9315a6c75ac6d895fa9b80bca
TiagoIRap <tiago.raposo@indiecampers.com>
2024-10-31 11:44:51 +0000

    main js changes

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import App from "./App.vue";
+import router from "./router";
 
 const vuetify = createVuetify({
-  components,
-  directives,
-})
+	components,
+	directives,
+	icons: {
+		iconfont: "mdi",
+	},
+});
 
-const app = createApp(App).use(vuetify)
+const app = createApp(App).use(vuetify);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
